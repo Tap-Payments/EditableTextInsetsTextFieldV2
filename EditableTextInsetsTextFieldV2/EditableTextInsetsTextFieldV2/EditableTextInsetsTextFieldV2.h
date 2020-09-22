318de71d203c37727cbfd60d8e6796e7b1530b09
O Rabie <o.rabie@tap.company>
2020-09-22 16:45:00 +0200

    Upgrade syntax to match the compiler in Xcode 12+

---
+++ EditableTextInsetsTextFieldV2/EditableTextInsetsTextFieldV2/EditableTextInsetsTextFieldV2.h
@@ -0,0 +1,18 @@
+//
+//  EditableTextInsetsTextFieldV2.h
+//  EditableTextInsetsTextFieldV2
+//
+//  Created by Osama Rabie on 9/22/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EditableTextInsetsTextFieldV2.
+FOUNDATION_EXPORT double EditableTextInsetsTextFieldV2VersionNumber;
+
+//! Project version string for EditableTextInsetsTextFieldV2.
+FOUNDATION_EXPORT const unsigned char EditableTextInsetsTextFieldV2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EditableTextInsetsTextFieldV2/PublicHeader.h>
+
+
